bcd0b1ea6ced82a5319171b958d7a1dd658fdf7e
Roberto Vargas Rojas <robertovargasrojas97@gmail.com>
2020-08-20 14:59:55 -0600

    Verifying MAX_NUM_FORMS (ready)

--- djgentelella/static/gentelella/js/custom_formset.js
+++ djgentelella/static/gentelella/js/custom_formset.js
@@ -4,19 +4,19 @@
     const formset_list = document.querySelector("#formset-list");
     const formset_template = document.querySelector("#formset-template").innerHTML;
     const add_button = document.querySelector("#add-form");
+    const error_msj = document.querySelector("#error-msj");
 
     // Control variables
     //Formset management inputs
     let [TOTAL_FORMS_INPUT, INITIAL_FORMS_INPUT, MIN_NUM_FORMS_INPUT, MAX_NUM_FORMS_INPUT] = $("#formset-management").find('input');
     //Values of each formset management input
     let [TOTAL_FORMS, INITIAL_FORMS, MIN_NUM_FORMS, MAX_NUM_FORMS] = [TOTAL_FORMS_INPUT.value, INITIAL_FORMS_INPUT.value, MIN_NUM_FORMS_INPUT.value, MAX_NUM_FORMS_INPUT.value];
-    
+
     // ------------------------ FUNCTIONS ------------------------
 
     //It's necessary because when the page is refreshed the total number of forms still same as the last
     const refresh_forms_number = () => {
         const forms_number = formset_list.childElementCount; //Returns the number of forms that formset_list contains
-        console.log(forms_number);
         update_management_information('reset', forms_number);
     };
 
@@ -29,35 +29,42 @@
             // TODO
         }
         else if (operation == 'reset') {
-            console.log("Entreee");
             TOTAL_FORMS_INPUT.value = current_number;
             TOTAL_FORMS = current_number;
         }
     };
 
-    const verify_max_num_forms = () => {
-        console.log(MAX_NUM_FORMS);
-        console.log(TOTAL_FORMS);
+    const can_add_forms = () => {
+        let can_add = ((TOTAL_FORMS + 1) > MAX_NUM_FORMS) ? false : true;
+
+        return can_add;
     };
 
     const create_new_form = (next_form_number) => {
         const new_form = document.createElement('div');
         new_form.setAttribute("id", `${formset_prefix}-${next_form_number}`);
         new_form.innerHTML = formset_template.replace(/__prefix__/gi, next_form_number);
+
         return new_form;
     };
 
     const add_new_form = (next_form_number) => {
-        verify_max_num_forms();
         const new_form = create_new_form(next_form_number);
         formset_list.append(new_form);
     };
 
     // ------------------------ EVENTS ------------------------
     const events = () => {
+
         add_button.addEventListener('click', () => {
-            add_new_form(TOTAL_FORMS);
-            update_management_information('add');
+            if (can_add_forms()) {
+                add_new_form(TOTAL_FORMS);
+                update_management_information('add');
+            }
+            else {
+                error_msj.innerHTML = `You can add only ${MAX_NUM_FORMS} forms`;
+            }
+
         });
 
         /*document.getElementsByName("deleteBtn").addEventListener("click", () => {
